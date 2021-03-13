e48faf271f55b77acb757247cde3c10bbe58fb98
LilWingXYZ <33993267+LilWingXYZ@users.noreply.github.com>
2021-03-13 21:56:28 +0800

    Create Context-Gated Convolution （backbone：ResNet50）.py

---
+++ Nets/Context-Gated Convolution （backbone：ResNet50）.py
@@ -0,0 +1,259 @@
+'''
+卷积注意力模块
+'''
+class Conv2d(nn.Conv2d):
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True):
+        super(Conv2d, self).__init__(in_channels, out_channels, kernel_size, stride,
+                 padding, dilation, groups, bias)
+        # for convolutional layers with a kernel size of 1, just use traditional convolution
+        if kernel_size == 1:
+            self.ind = True
+        else:
+            self.ind = False            
+            self.oc = out_channels
+            self.ks = kernel_size
+            
+            # the target spatial size of the pooling layer
+            ws = kernel_size
+            self.avg_pool = nn.AdaptiveAvgPool2d((ws,ws))
+            
+            # the dimension of the latent repsentation
+            num_lat = int((kernel_size * kernel_size) / 2 + 1)
+            
+            # the context encoding module
+            self.ce = nn.Linear(ws*ws, num_lat, False)            
+            self.ce_bn = nn.BatchNorm1d(in_channels)
+            self.ci_bn2 = nn.BatchNorm1d(in_channels)
+            
+            # activation function is relu
+            self.act = nn.ReLU(inplace=True)
+            
+            
+            # the number of groups in the channel interacting module
+            if in_channels // 16:
+                self.g = 16
+            else:
+                self.g = in_channels
+            # the channel interacting module    
+            self.ci = nn.Linear(self.g, out_channels // (in_channels // self.g), bias=False)
+            self.ci_bn = nn.BatchNorm1d(out_channels)
+            
+            # the gate decoding module
+            self.gd = nn.Linear(num_lat, kernel_size * kernel_size, False)
+            self.gd2 = nn.Linear(num_lat, kernel_size * kernel_size, False)
+            
+            # used to prrepare the input feature map to patches
+            self.unfold = nn.Unfold(kernel_size, dilation, padding, stride)
+            
+            # sigmoid function
+            self.sig = nn.Sigmoid()
+    def forward(self, x):
+        # for convolutional layers with a kernel size of 1, just use traditional convolution
+        if self.ind:
+            return F.conv2d(x, self.weight, self.bias, self.stride,
+                        self.padding, self.dilation, self.groups)
+        else:
+            b, c, h, w = x.size()
+            weight = self.weight
+            # allocate glbal information
+            gl = self.avg_pool(x).view(b,c,-1)
+            # context-encoding module
+            out = self.ce(gl)
+            # use different bn for the following two branches
+            ce2 = out
+            out = self.ce_bn(out)
+            out = self.act(out)
+            # gate decoding branch 1
+            out = self.gd(out)
+            # channel interacting module
+            if self.g >3:
+                # grouped linear
+                oc = self.ci(self.act(self.ci_bn2(ce2).\
+                                      view(b, c//self.g, self.g, -1).transpose(2,3))).transpose(2,3).contiguous()
+            else:
+                # linear layer for resnet.conv1
+                oc = self.ci(self.act(self.ci_bn2(ce2).transpose(2,1))).transpose(2,1).contiguous() 
+            oc = oc.view(b,self.oc,-1) 
+            oc = self.ci_bn(oc)
+            oc = self.act(oc)
+            # gate decoding branch 2
+            oc = self.gd2(oc)   
+            # produce gate
+            out = self.sig(out.view(b, 1, c, self.ks, self.ks) + oc.view(b, self.oc, 1, self.ks, self.ks))
+            # unfolding input feature map to patches
+            x_un = self.unfold(x)
+            b, _, l = x_un.size()
+            # gating
+            out = (out * weight.unsqueeze(0)).view(b, self.oc, -1)
+            # currently only handle square input and output
+            return torch.matmul(out,x_un).view(b, self.oc, int(np.sqrt(l)), int(np.sqrt(l)))  
+
+
+'''
+backbone:ResNet50
+'''
+__all__ = ['cgc_resnet50']
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 context gated convolution with padding"""
+    return Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = conv1x1(inplanes, planes)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = conv3x3(planes, planes, stride)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = conv1x1(planes, planes * self.expansion)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=2, zero_init_residual=False):
+        super(ResNet, self).__init__()
+        self.inplanes = 64
+        self.conv1 = Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(Bottleneck, 64, layers[0])
+        self.layer2 = self._make_layer(Bottleneck, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+    
+
+    def forward(self, x, my=False, ft=False):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+
+def cgc_resnet50(pretrained=False, **kwargs):
+    """Constructs a CGC-ResNet-50 model.
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    return model
